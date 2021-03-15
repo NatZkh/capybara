ee4a6118360df213adb8d1a9d28636e293bb1104
NatZkh <lia.nickson1@gamil.com>
2021-03-15 14:31:52 +0100

    add jupyter notebook to capybuntu container

--- dockers/run-docker.bat
+++ dockers/run-docker.bat
@@ -1,2 +1,2 @@
 : script for running docker
-docker run -v "D:\Programming stuff\capybara:/capybara" -it --rm capybuntu bash
+docker run -v "D:\Programming stuff\capybara:/capybara" -p 8888:8888 -it --rm capybuntu bash
