4f2eeac2bb19707c9c2f3d3fe59edca75268c0a6
NatZkh <lia.nickson1@gamil.com>
2021-03-15 18:11:49 +0100

    make mounted directory work directory

--- dockers/run-docker.bat
+++ dockers/run-docker.bat
@@ -1,2 +1,2 @@
 : script for running docker
-docker run -v "D:\Programming stuff\capybara:/capybara" -p 8888:8888 -it --rm capybuntu bash
+docker run -it --rm -v "D:\Programming stuff:/programming" -p 8888:8888 capybuntu bash
