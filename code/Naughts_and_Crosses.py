3236d2402dc9578633ce4f2da70cc85603264747
NatZkh <lia.nickson1@gamil.com>
2021-04-13 21:59:22 +0200

    defined functions for player1 and player2 iputs and function to update game status

--- code/Naughts_and_Crosses.py
+++ code/Naughts_and_Crosses.py
@@ -11,4 +11,20 @@ def print_game_state(game_state):
        + "—+—+—\n" 
        + game_state[2][0] + "|" + game_state[2][1] + "|" + game_state[2][2])
 
-       
+
+def player1_input():
+    row = int(input ("Player 1, please input row number: "))
+    col = int(input ("Player 1, please inout column number: "))
+    p1_input = ("x", row, col)
+    return p1_input
+
+def player2_input():
+    row = int (input ("Player 2, please input row number: "))
+    col = int (input ("Player 2, please inout column number: "))
+    p2_input = ("o", row, col)
+    return p2_input
+
+def update_game_status(game_state, game_command):
+    game_state[game_command[1]][game_command[2]] = game_command[0]
+    return game_state
+
